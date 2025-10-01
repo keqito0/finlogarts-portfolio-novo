f74519e3f0802e0af7062a5e081280b8b183087c
keqito0 <144688198+keqito0@users.noreply.github.com>
2025-10-01 20:26:46 -0300

    Update scatter_ticket_frequencia.sql

--- docs/sql/scatter_ticket_frequencia.sql
+++ docs/sql/scatter_ticket_frequencia.sql
@@ -1,1 +1,22 @@
+WITH pedidos_por_cliente AS (
+  SELECT
+    o.customer_id,
+    COUNT(DISTINCT o.order_id) AS qtd_pedidos,
+    ROUND(SUM(oi.price * oi.quantity), 2) AS receita_total,
+    ROUND(SUM(oi.price * oi.quantity) / COUNT(DISTINCT o.order_id), 2) AS ticket_medio
+  FROM `meli-bi-data.TMP.orders_kq` o
+  JOIN `meli-bi-data.TMP.order_items_kq` oi ON o.order_id = oi.order_id
+  WHERE o.status = 'Entregue'
+    AND o.order_date BETWEEN '2024-01-01' AND '2024-12-31'
+  GROUP BY o.customer_id
+)
+SELECT
+  c.customer_id,
+  c.name AS customer_name,
+  c.region,
+  qtd_pedidos AS frequencia_compra,
+  ticket_medio,
+  receita_total
+FROM pedidos_por_cliente pc
+JOIN `meli-bi-data.TMP.customers_kq` c  ON pc.customer_id = c.customer_id;
 
