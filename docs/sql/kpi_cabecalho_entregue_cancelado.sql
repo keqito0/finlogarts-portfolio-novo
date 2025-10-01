2b0e1ec85035beb8b07eb91ef51d3f5a91d650e6
keqito0 <144688198+keqito0@users.noreply.github.com>
2025-10-01 20:25:15 -0300

    Update kpi_cabecalho_entregue_cancelado.sql

--- docs/sql/kpi_cabecalho_entregue_cancelado.sql
+++ docs/sql/kpi_cabecalho_entregue_cancelado.sql
@@ -1,1 +1,17 @@
 
+
+WITH base AS (
+  SELECT
+    c.region AS region,
+    o.status
+  FROM `meli-bi-data.TMP.orders_kq` o
+  LEFT JOIN `meli-bi-data.TMP.customers_kq` c
+    ON c.customer_id = o.customer_id
+)
+SELECT
+  region,
+  ROUND(100 * SAFE_DIVIDE(COUNTIF(status = 'Entregue'), COUNT(*)), 2) AS pct_entregue,
+  ROUND(100 * SAFE_DIVIDE(COUNTIF(status = 'Cancelado'), COUNT(*)), 2) AS pct_cancelado
+FROM base
+GROUP BY region
+ORDER BY region;
