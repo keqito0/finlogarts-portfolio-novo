0e33cdddbf518ea4ea7ecf797b00b97055cd14ec
keqito0 <144688198+keqito0@users.noreply.github.com>
2025-10-01 20:28:03 -0300

    Update ticket_medio_regiao.sql

--- docs/sql/ticket_medio_regiao.sql
+++ docs/sql/ticket_medio_regiao.sql
@@ -1,1 +1,19 @@
 
+WITH base AS (
+  SELECT
+    o.order_id,
+    c.region,
+    SUM(oi.price * oi.quantity) AS total_pedido
+  FROM `meli-bi-data.TMP.order_items_kq` oi
+  LEFT JOIN `meli-bi-data.TMP.orders_kq` o
+    ON oi.order_id = o.order_id
+  LEFT JOIN `meli-bi-data.TMP.customers_kq` c
+    ON o.customer_id = c.customer_id
+  GROUP BY o.order_id, c.region
+)
+SELECT
+  region,
+  ROUND(AVG(total_pedido)) AS ticket_medio
+FROM base
+GROUP BY region
+ORDER BY ticket_medio DESC;
